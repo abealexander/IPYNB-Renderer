4f12ab816e4f6f891d494f847c4bfa952778cdaa
abealexander07 <abealexander07@gmail.com>
2022-09-26 17:42:40 +0530

    Setup of Local Package

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL
-    description="A small python package"
+    author_email=AUTHOR_EMAIL,
+    description="A small python package",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github,com/{AUTHOR_USER_NAME}/{REPO_NAME}"
+    url=f"https://github,com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github,com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
     },
