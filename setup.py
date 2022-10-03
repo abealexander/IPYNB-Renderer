caf8e8f8e6aea4f40471e1dc6b9d5e430e23c310
abealexander07 <abealexander07@gmail.com>
2022-10-03 15:42:29 +0530

    Name Change Commit

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
-REPO_NAME = "IPYNBRenderer"
+REPO_NAME = "IPYNB-Renderer"
 AUTHOR_USER_NAME = "abealexander"
-SRC_REPO = "IPYNBRenderer"
+SRC_REPO = "IPYNB0-Renderer"
 AUTHOR_EMAIL = "abealexander07@gmail.com"
 
 setuptools.setup(
